137f890f0706cb024eb70bc4d6cd0f9b40d050a1
Alencar Rodrigo Hentges <alencarhentges@gmail.com>
2017-12-18 21:12:34 -0200

    ajustes na identificação da complementação nominal

--- complemento_nominal/complemento_nominal.py
+++ complemento_nominal/complemento_nominal.py
@@ -14,8 +14,7 @@ def complementos_nominais(frase):
         execute = DNs[palavra.tagInicial] if palavra.tagInicial in DNs else None
         if(execute):
             element = execute(frase, palavra)
-            if(element):
-                lista.append(element)
+            lista.append(element)
 
     return lista
 
@@ -23,7 +22,7 @@ def complementos_nominais(frase):
 def dn_adj(frase, palavra):
     complemento = palavra
     arvore_que_estou = get_arvore_que_estou(frase, palavra)
-    nome = get_nucleo(arvore_que_estou)
+    nome = find_nucleo_arvore(arvore_que_estou)
 
     return build_response(nome, complemento)
 
@@ -31,25 +30,17 @@ def dn_prop(frase, palavra):
     return dn_adj(frase, palavra)
 
 def dn_np(frase, palavra):
-    print("executando DN_NP")
-
     minha_arvore = get_minha_arvore(frase, palavra)
-    minha_arvore_nucleo = get_nucleo(minha_arvore)
-
-    arvore_no_nao_terminal_nucleo = get_nucleo_sub_arvores(minha_arvore)
-
-    return build_response(minha_arvore_nucleo, arvore_no_nao_terminal_nucleo)
-
-
-
-def dn_pp(frase, palavra):
-    print("executando DN_PP")
-    minha_arvore = get_minha_arvore(frase, palavra)
-
+    minha_arvore_nucleo = find_nucleo_arvore(minha_arvore)
 
     arvore_que_estou = get_arvore_que_estou(frase, palavra)
-    arvore_que_estou_nucleo = get_nucleo(arvore_que_estou)
+    arvore_que_estou_nucleo = find_nucleo_arvore(arvore_que_estou)
 
+    return build_response(arvore_que_estou_nucleo, minha_arvore_nucleo)
+
+def dn_pp(frase, palavra):
+    arvore_que_estou = get_arvore_que_estou(frase, palavra)
+    arvore_que_estou_nucleo = find_nucleo_arvore(arvore_que_estou)
     arvore_no_nao_terminal_nucleo = get_nucleo_sub_arvores(arvore_que_estou)
 
     return build_response(arvore_que_estou_nucleo, arvore_no_nao_terminal_nucleo)
@@ -65,19 +56,19 @@ DNs = {
 
 
 def get_nucleo_sub_arvores(arvore):
-    arvore_no_nao_terminal = get_minha_arvore(arvore, get_no_nao_terminal(arvore))
+    arvore_no_nao_terminal = get_minha_arvore(arvore, find_nodo_nao_terminal(arvore))
     arvore_no_nao_terminal_nucleo = None
     while (arvore_no_nao_terminal):
-        arvore_no_nao_terminal_nucleo = get_nucleo(arvore_no_nao_terminal)
+        arvore_no_nao_terminal_nucleo = find_nucleo_arvore(arvore_no_nao_terminal)
         if (arvore_no_nao_terminal_nucleo):
             break
 
-        arvore_no_nao_terminal = get_minha_arvore(arvore_no_nao_terminal, get_no_nao_terminal(arvore_no_nao_terminal))
+        arvore_no_nao_terminal = get_minha_arvore(arvore_no_nao_terminal, find_nodo_nao_terminal(arvore_no_nao_terminal))
 
     return arvore_no_nao_terminal_nucleo
 
 
-def get_no_nao_terminal(frase):
+def find_nodo_nao_terminal(frase):
     for palavra in frase.palavras[1:]:
         if(FraseTreeUtil.isNoNaoTerminal(frase, palavra.numero)):
             return palavra
@@ -106,10 +97,10 @@ def get_minha_arvore(frase, palavra):
     return Frase(minha_arvore, frase.id)
 
 def get_arvore_que_estou(frase, palavra):
-    palavra_pai = get_pai(frase, palavra)
+    palavra_pai = find_pai(frase, palavra)
     return get_minha_arvore(frase, palavra_pai)
 
-def get_pai(frase, palavra):
+def find_pai(frase, palavra):
     palavras = frase.palavras
     index = palavras.index(palavra)
 
@@ -121,8 +112,7 @@ def get_pai(frase, palavra):
 
     return None
 
-def get_nucleo(frase):
-
+def find_nucleo_arvore(frase):
     for nucleo in NUCLEOS:
         for palavra in frase.palavras:
             if nucleo in palavra.tagInicial:
