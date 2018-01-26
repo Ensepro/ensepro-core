dd7bf05e91f93fb79cfe9c88509144d02c8d1e3b
Alencar Rodrigo Hentges <alencarhentges@gmail.com>
2018-01-26 21:15:03 -0200

    fix arvore printada com ordenação errada das palavras

--- ensepro/conversores/arvore_conversor.py
+++ ensepro/conversores/arvore_conversor.py
@@ -7,9 +7,44 @@
 from ensepro.arvore import Arvore
 
 
+def __corrige_problema_ordenacao_arvore_printada(palavra):
+    """
+    Este problema ocorre quando a palavra não possui uma palavra_original e não possui filhos,
+    como no exemplo das sub-arvores (ex:X:par|2|, também não possui palavra_original, mas possui filhos).
+
+    Quando uma palavra não possui palavra_original, o termo se torna uma flhos, e o print feito
+    pela nltk.tree.Tree faz algum tipo de ordenação e printa primeiro as folhas, e depois as sub-arvores.
+
+    Frase usada para exemplo: "Alencar, tudo bem?"
+    Sem correção: ---------------------------------------------------------------
+                               UTT:x|1|
+                       ___________|____________
+                   X:par|2|
+              ________|___________         ____|______
+             |   CJT:prop|3| CJT:spec|5|  |       fA:adv|6|
+             |        |           |       |           |
+            ,|4|   Alencar       tudo    ?|7|        bem
+
+    Com correção: ---------------------------------------------------------------
+                               UTT:x|1|
+                        __________|_________________
+                    X:par|2|
+              _________|__________            ______|___
+        CJT:prop|3|   ,|4|   CJT:spec|5| fA:adv|6|     ?|7|
+             |         |          |          |          |
+          Alencar     ...        tudo       bem        ...
+    """
+    if palavra.palavra_original:
+        return
+
+    CARACTERES_PARA_ADICIONAR_ESPACO = ",.;?!\""
+
+    if palavra.tag_inicial in CARACTERES_PARA_ADICIONAR_ESPACO:
+        palavra.palavra_original = ' '
+
+
 def from_frase(frase):
     arvore = Arvore()
-    limpar_ultima_palavra = False
     ultimo_node_adicionado = None
     index = 0
     numero_de_palavras = len(frase.palavras)
@@ -18,14 +53,11 @@ def from_frase(frase):
     while (index < numero_de_palavras):
         palavra = frase.palavras[index]
 
-        if (index + 1) == numero_de_palavras \
-                and palavra.tag_inicial \
-                and not palavra.palavra_original:
-            # Se a última palavra somente possuir tag_inicial(e tiver a palavra_original vazia), a árvore gerada pelo nltk.tree.Tree
-            # coloca o nó dessa última palavra na primeira subarvore, antes das demais palavras.
-            # A solução é colocar a palavra_original com um espaço
-            palavra.palavra_original = " "
-            limpar_ultima_palavra = True
+
+        # Quando ocorre algum palavra sem palavra_original e com tag_inicial, a ordem de aparição na arvore printada
+        # pelo nltk.tree.Tree fica errada, sendo esta palavra printada antes das demais.
+        # O método abaixo corrige este problema e informa uma melhor explicação
+        __corrige_problema_ordenacao_arvore_printada(palavra)
 
         # Se possui um último node adicionado, indica que a palavra atual pode ser filho deste node
         if ultimo_node_adicionado:
@@ -45,11 +77,5 @@ def from_frase(frase):
         else:
             ultimo_node_adicionado = arvore.adicionar_node(palavra.id, palavra)
             index += 1
-
-    # Comentado pois como o objeto é o mesmo, se a árvore for printada novamente depois, não terá mais o espaço para corrigir
-    # 
-    # if limpar_ultima_palavra:
-    #     # Caso foi adicionado um espaço na palavra_original da última palavra, é feito sua remoção antes de seguir
-    #     frase.palavras[numero_de_palavras - 1].palavra_original = ''
 
     return arvore
