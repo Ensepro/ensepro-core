d6ef1a30fcafd9d726f105cd0cc963743f1e8cfc
Alencar Rodrigo Hentges <alencarhentges@gmail.com>
2018-05-19 01:16:38 -0300

    Ajuste na lógica do attention e outros pequenos ajustes gerais. resolving #30.

--- ensepro/servicos/palavras_service.py
+++ ensepro/servicos/palavras_service.py
@@ -7,6 +7,8 @@
 """
 
 import requests
+from requests import HTTPError
+
 import ensepro.configuracoes as configuracoes
 from ensepro.constantes import PalavrasServidorConstantes as ps_consts, LoggerConstantes as logger_consts
 
@@ -17,6 +19,9 @@ porta = configuracoes.get_config(ps_consts.PORTA)
 servico_analisar_frase = configuracoes.get_config(ps_consts.SERVICO_ANALISAR_FRASE)
 
 
+# TODO verificar a necessidade de uma classe de Request e Response
+# TODO 2. futuro: implementar no PalavrasService um serviço que recebe uma lista de frase
+#           e faz a analise delas e retorna tudo de uma vez... (acho que fica melhor do que ficar chamando toda hora...)
 def analisar_frase(frase: str):
     logger.info("Analisando frase: [frase=%s]", frase)
 
@@ -31,11 +36,9 @@ def analisar_frase(frase: str):
         logger.debug("Response as json: [response=%s]", response.json())
         return response
 
-    # Se respose não OK, lança exception
-    exception = Exception("Erro ao analisar frase: [status_code={0}, reason={1}, response_text={2}]" \
-                          "".format(response.status_code, response.reason, response.text))
-
-    logger.exception(exception, exc_info=False)
+    # Se respose não OK, throw exception
+    exception = HTTPError("Erro ao chamar o serviço PalavrasService: [status_code={0}, reason={1}]" \
+                          "".format(response.status_code, response.reason), response=response)
     raise exception
 
 
